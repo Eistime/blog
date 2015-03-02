15690fe1798361e8a11b6eeb094c8828078b20c3
Carlos Jimenez ruiz <cjr.creativo@gmail.com>
2015-03-02 21:11:58 +0000

    correction of spelling mistakes

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ articles = [
   },
   {
     :tag => "convention, london, tattoo",
-    :title => "Internation Tattoo Convention, London",
+    :title => "International Tattoo Convention, London",
     :date => "10/09/2014",
     :content => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum magna quis nisi sagittis sollicitudin. Phasellus vel urna accumsan libero commodo hendrerit sed et orci. Duis libero tortor, aliquam eu egestas eu, porta et diam. Nunc tincidunt scelerisque mauris, vitae ullamcorper diam luctus vel. Nam suscipit condimentum justo quis congue. Nullam lobortis mattis diam ac viverra. Nunc lobortis augue eu augue pretium, non dapibus purus tristique. Mauris aliquet ante in turpis posuere, sit amet varius leo luctus. Donec nec ipsum urna. Duis fermentum eros pulvinar lorem varius, id fringilla diam convallis. Vivamus eu lacus eros. Aliquam malesuada sem in diam pharetra rutrum. Curabitur sollicitudin eget elit at tempor. Pellentesque ac tellus in nulla viverra accumsan.",
   }
@@ -29,6 +29,8 @@ get '/' do
 end
 
 get '/article' do
+
   id = params[:id].to_i
   erb :article_template, :locals => {:article => articles[id], :articles => articles}
+
 end
